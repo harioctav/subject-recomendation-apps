b972b876a1e31e65e417dbe26ce64ce2f642eadd
Qlok Apps Dev <qlokappsdev@Qloks-MacBook-Pro.local>
2024-09-05 21:23:40 +0700

    update: create modul path js in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/js/app.js",
+                "resources/js/home.js",
+
+                // Academics
+                "resources/js/academics/major_subjects/index.js",
+                "resources/js/academics/majors/index.js",
+                "resources/js/academics/students/index.js",
+                "resources/js/academics/students/input.js",
+                "resources/js/academics/subjects/index.js",
+
+                // Evaluations
+                "resources/js/evaluations/grades/index.js",
+                "resources/js/evaluations/grades/input.js",
+                "resources/js/evaluations/recommendations/index.js",
+                "resources/js/evaluations/recommendations/create.js",
+                "resources/js/evaluations/recommendations/show.js",
+
+                // Settings
+                "resources/js/settings/roles/index.js",
+                "resources/js/settings/roles/input.js",
+                "resources/js/settings/users/index.js",
+
+                // Utils
+                "resources/js/utils/tooltip.js",
             ],
             refresh: true,
         }),
